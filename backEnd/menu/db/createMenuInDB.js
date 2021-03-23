62404ef90b5bb9df1b4acf78c2cc1c38b1cd550f
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-03-23 20:16:02 +0200

    implement drop menu collection

--- backEnd/menu/db/createMenuInDB.js
+++ backEnd/menu/db/createMenuInDB.js
@@ -1,3 +1,4 @@
+const dropMenuCollection = require('./dropMenuCollection');
 const menu = require('../menus');
 const config = require('../../../config');
 const InsertDB = require('../../db/InsertDB');
@@ -5,6 +6,7 @@ const ConnectMongoDB = require('../../db/connectMongoDB.js');
 const mongo = new ConnectMongoDB();
 
 async function createMenuInDB() {
+  await dropMenuCollection();
   const insertMethod = new InsertDB(mongo);
   await insertMethod.connect(config.db.name);
   const collectionName = config.collections.menu.name;
