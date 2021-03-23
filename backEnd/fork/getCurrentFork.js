1b75aa3d0fa1f6cac91c72856da9b521d1782c3a
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-03-23 20:51:10 +0200

    rename file

--- backEnd/fork/getCurrentFork.js
+++ backEnd/fork/getCurrentFork.js
@@ -1,11 +1,11 @@
 const { getStatistic } = require('../statistic/db');
 const { getForkByParseCount } = require('./db');
 
-async function getLastFork() {
+async function getCurrentFork() {
   const stat = await getStatistic();
   const { parseCount } = stat;
   const result = await getForkByParseCount(parseCount);
   return result;
 }
 
-module.exports = getLastFork;
+module.exports = getCurrentFork;
--- backEnd/fork/handlerGetFork.js
+++ backEnd/fork/handlerGetFork.js
@@ -3,7 +3,7 @@ const { incrementStatistic } = require('../statistic/db');
 const { checkFork, addStatisticToForkResult } = require('../../parser/methods');
 const db = require('../../parser/methods/db');
 
-const getLastFork = require('./getLastFork');
+const getCurrentFork = require('./getCurrentFork');
 const { getBkById } = require('../bk');
 
 async function handlerGetFork(data, UserOnline) {
@@ -13,7 +13,7 @@ async function handlerGetFork(data, UserOnline) {
   // let forkResult = await checkFork(true);
   // forkResult = addStatisticToForkResult(forkResult, statistic);
   // await db.addForkResultToDB(forkResult);
-  const result = await getLastFork();
+  const result = await getCurrentFork();
   const ids = formBkIdArr(result);
   const bk = await getBkById(Array.from(ids));
   const message = {
