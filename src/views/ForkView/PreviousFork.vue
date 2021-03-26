0e2eec81ad4e3e6b3da01f0f76d755ea80c59129
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-03-26 22:13:26 +0200

    create template get previous fork

--- src/views/ForkView/PreviousFork.vue
+++ src/views/ForkView/PreviousFork.vue
@@ -1,15 +1,35 @@
 <template>
-	<div>
-PreviousFork
-	</div>
+  <div class="previos-fork">
+    <div class="previos-fork__title">Недавние вилки</div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name:"PreviousFork"
-	}
+export default {
+  name: 'PreviousFork',
+  created() {
+    this.getPreviosFork();
+  },
+  methods: {
+    async getPreviosFork() {
+      if (this.$data.$api.wsInstance.readyState !== 1) {
+        setTimeout(() => {
+          this.getPreviosFork();
+        }, 100);
+      } else {
+        const response = await this.$data.$api.get({ type: '/getPreviousFork' });
+				console.log(response);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-
+.previos-fork {
+  &__title {
+    font-weight: bold;
+    font-size: calc(var(--base-font-size) * 1.6);
+  }
+}
 </style>
