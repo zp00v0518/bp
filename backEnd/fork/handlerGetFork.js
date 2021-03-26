5e3089f8a20e2b4ad282f15212b288120b92f26a
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-03-26 20:22:56 +0200

    add comment

--- backEnd/fork/handlerGetFork.js
+++ backEnd/fork/handlerGetFork.js
@@ -1,10 +1,11 @@
 const { sendWSMessage } = require('../wsServer');
+
+// закомментируемый код проверяет вилки непосредственно в событиях 
 // const { incrementStatistic } = require('../statistic/db');
 // const { checkFork, addStatisticToForkResult } = require('../../parser/methods');
 // const db = require('../../parser/methods/db');
 
 const getCurrentFork = require('./getCurrentFork');
-// const { getBkById } = require('../bk');
 
 async function handlerGetFork(data, UserOnline) {
   const { ws } = UserOnline;
@@ -14,28 +15,13 @@ async function handlerGetFork(data, UserOnline) {
   // forkResult = addStatisticToForkResult(forkResult, statistic);
   // await db.addForkResultToDB(forkResult);
   const result = await getCurrentFork();
-  // const ids = formBkIdArr(result);
-  // const bk = await getBkById(Array.from(ids));
   const message = {
     type: data.type,
     data: {
       result
-      // bk
     }
   };
   sendWSMessage(ws, message);
 }
-
-// function formBkIdArr(arr) {
-//   const z = new Map();
-//   arr.forEach((item) => {
-//     const fork = item.fork;
-//     fork.forEach((el) => {
-//       z.set(el.firstBk.bkId, el.firstBk.bkId);
-//       z.set(el.secondBk.bkId, el.secondBk.bkId);
-//     });
-//   });
-//   return z.values();
-// }
 
 module.exports = handlerGetFork;
