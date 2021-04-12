de45754ff32a541dbaffe4b45112694a09978d72
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-04-12 21:50:04 +0300

    фывФЫВ

--- backEnd/tournament/init/tournamentsList.js
+++ backEnd/tournament/init/tournamentsList.js
@@ -1,4 +1,13 @@
-const sport_category = require('../../sportCategory/sport_category');
-const tournaments ={
+const sport = require('../../sportCategory/init/sport_category');
+const list = require('../list');
+const tournaments = Object.assign({}, sport);
+Object.keys(tournaments).forEach((key) => {
+  delete tournaments[key].name;
+  tournaments[key].list = [];
+});
 
-}
+tournaments[sport.soccer.key].list = list.soccer;
+tournaments[sport.basketball.key].list = list.basketball;
+console.log(tournaments);
+
+module.exports = tournaments;
